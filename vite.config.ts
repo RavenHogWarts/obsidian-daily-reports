675360ce3b60520ec923a8c1c02bb98433381d1d
RavenHogwarts <fengyj120@163.com>
2026-01-13 19:56:26 +0800

    feat: 修改图标，并引入tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "node:fs";
 import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 
 // Plugin to generate index.json file at build start
 const generateIndexFilePlugin = () => {
@@ -52,5 +53,5 @@ const generateIndexFilePlugin = () => {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), generateIndexFilePlugin()],
+  plugins: [react(), tailwindcss(), generateIndexFilePlugin()],
 });
